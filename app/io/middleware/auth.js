3e7e31dbc36c20ae6f72c65b71329d8a95c68521
caiwenduo <caiwenduo@163.com>
2019-10-23 11:00:40 +0800

    add

--- app/io/middleware/auth.js
+++ app/io/middleware/auth.js
@@ -1,15 +1,15 @@
-'use strict'
+'use strict';
 module.exports = () => {
   return async (ctx, next) => {
     // ctx.socket.emit('res', 'connected!');
-    console.log(66)
-    ctx.socket.join('room')
+    console.log(66);
+    ctx.socket.join('room');
     ctx.socket.on('login', function(res) {
-      console.log('login', res)
-    })
+      console.log('login', res);
+    });
     // console.log('ctx.socket.adapter', ctx.socket.adapter);
-    ctx.socket.adapter.clients(['room'], (err, clients) => {
-      console.log('clients', clients)
+    ctx.socket.adapter.clients([ 'room' ], (err, clients) => {
+      console.log('clients', clients);
       // logger.debug('#online_join', clients);
       // // 更新在线用户列表
       // nsp.to(room).emit('online', {
@@ -18,11 +18,11 @@ module.exports = () => {
       //   target: 'participator',
       //   message: `User(${id}) joined.`,
       // });
-    })
+    });
     // ctx.socket.on('login', res => {
     //   console.log('login', res);
     // });
     // ctx.socket.disconnect();
-    await next()
-  }
-}
+    await next();
+  };
+};
