6ccf283facf6055036a6bb2d7004f718769d0f57
caiwenduo <cawenduo@163.com>
2019-10-27 12:44:05 +0800

    add

--- app/router.js
+++ app/router.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller, io } = app;
-  router.get('/', controller.home.index);
+  const { router, controller, io } = app
+  router.get('/', controller.home.index)
 
   // socket.io
-  io.of('/').route('exchange', io.controller.nsp.exchange);
-};
+  io.of('/').route('exchange', io.controller.nsp.exchange)
+}
