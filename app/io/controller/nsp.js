d3bff237a055e817a9e16b6bb8e9c5f634247ff5
caiwenduo <caiwenduo@163.com>
2019-10-31 15:48:38 +0800

    add

--- app/io/controller/nsp.js
+++ app/io/controller/nsp.js
@@ -1,27 +1,27 @@
-'use strict'
+'use strict';
 
-const Controller = require('egg').Controller
+const Controller = require('egg').Controller;
 
 class NspController extends Controller {
   async exchange() {
-    console.log('exchange')
-    console.log('exchange')
-    console.log('exchange')
-    const { ctx, app } = this
-    const nsp = app.io.of('/')
-    const message = ctx.args[0] || {}
-    const socket = ctx.socket
-    const client = socket.id
+    console.log('exchange');
+    console.log('exchange');
+    console.log('exchange');
+    const { ctx, app } = this;
+    const nsp = app.io.of('/');
+    const message = ctx.args[0] || {};
+    const socket = ctx.socket;
+    const client = socket.id;
 
     try {
-      const { target, payload } = message
-      if (!target) return
-      const msg = ctx.helper.parseMsg('exchange', payload, { client, target })
-      nsp.emit(target, msg)
+      const { target, payload } = message;
+      if (!target) return;
+      const msg = ctx.helper.parseMsg('exchange', payload, { client, target });
+      nsp.emit(target, msg);
     } catch (error) {
-      app.logger.error(error)
+      app.logger.error(error);
     }
   }
 }
 
-module.exports = NspController
+module.exports = NspController;
