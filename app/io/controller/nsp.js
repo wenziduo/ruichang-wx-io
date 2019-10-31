95edf57864adb6437d240014af8f09826016ef98
caiwenduo <caiwenduo@163.com>
2019-10-31 15:48:21 +0800

    add

---
+++ app/io/controller/nsp.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const Controller = require('egg').Controller
+
+class NspController extends Controller {
+  async exchange() {
+    console.log('exchange')
+    console.log('exchange')
+    console.log('exchange')
+    const { ctx, app } = this
+    const nsp = app.io.of('/')
+    const message = ctx.args[0] || {}
+    const socket = ctx.socket
+    const client = socket.id
+
+    try {
+      const { target, payload } = message
+      if (!target) return
+      const msg = ctx.helper.parseMsg('exchange', payload, { client, target })
+      nsp.emit(target, msg)
+    } catch (error) {
+      app.logger.error(error)
+    }
+  }
+}
+
+module.exports = NspController
